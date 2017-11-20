e48bb2a7cd6cb24b70aff3216314f6d936206b11
teacheg <teacheg@hotmail.com>
2017-11-20 09:49:05 +0800

    first local commit lower-log

---
+++ lower-log/src/main/java/cn/com/taiji/log_only/App.java
@@ -0,0 +1,13 @@
+package cn.com.taiji.log_only;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
