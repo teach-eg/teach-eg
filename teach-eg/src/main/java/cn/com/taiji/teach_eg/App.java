e916758ec84b4318fd7805db4a1a5f416e266a01
teacheg <teacheg@hotmail.com>
2017-11-20 09:42:29 +0800

    first local commit

---
+++ teach-eg/src/main/java/cn/com/taiji/teach_eg/App.java
@@ -0,0 +1,13 @@
+package cn.com.taiji.teach_eg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
